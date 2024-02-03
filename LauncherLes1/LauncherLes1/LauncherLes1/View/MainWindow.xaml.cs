8227a666ab1b2c0dcb39e58d799cfdc7239ce24b
Maksym <mplakushko@gmail.com>
2024-02-03 08:11:34 +0100

    C#

---
+++ LauncherLes1/LauncherLes1/LauncherLes1/View/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LauncherLes1.View
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string opis;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //this.Reset();
+        }
+     /*   public void Reset()
+        {
+           // opis.Text = String.Empty;
+
+
+            methods.Items.Clear();
+            CheckBox method;
+            foreach (string methodName in ringingMethods)
+            {
+                method = new CheckBox();
+                method.Margin = new Thickness(0, 0, 0, 10);
+                method.Content = methodName;
+                methods.Items.Add(method);
+            }
+            isCaptain.IsChecked = false;
+            novice.IsChecked = true;
+            System.Windows.Forms.DateTimePicker memberDate =
+                hostMemberSince.Child as System.Windows.Forms.DateTimePicker;
+            memberDate.Value = DateTime.Today;
+
+        }*/
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ LauncherLes1/LauncherLes1/LauncherLes1/ViewModel/MainViewModel.cs
@@ -0,0 +1,88 @@
+﻿using GalaSoft.MvvmLight.Command;
+using LauncherLes1.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace LauncherLes1.ViewModel
+{
+    internal class MainViewModel : ViewModedBase
+    {
+        private Page WF = new WF();
+        private Page CL = new CL();
+        private Page HK = new HK();
+        private Page Hades = new Hades();
+        private Page POE = new POE();
+        private Page VL = new VL();
+        private Page MGV = new MGV();
+        private Page FC = new FC();
+        private Page _CurPage = new WF();
+        
+
+        public Page CurPage
+        {
+            get => _CurPage;
+            set => Set(ref _CurPage, value);
+        }
+        public ICommand OpenCLPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = CL);
+            }
+        }
+        public ICommand OpenWFPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = WF);
+            }
+        }
+        public ICommand OpenHKPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = HK);
+            }
+        }
+        public ICommand OpenHadesPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = Hades);
+            }
+        }
+        public ICommand OpenPOEPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = POE);
+            }
+        }
+        public ICommand OpenVLPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = VL);
+            }
+        }
+        public ICommand OpenMGVPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = MGV);
+            }
+        }
+        public ICommand OpenFCPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurPage = FC);
+            }
+        }
+    }
+}
